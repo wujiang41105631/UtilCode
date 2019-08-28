0587f8b536e0f52b05af4e042d6f930911714d6c
xupeng.guo <xupeng.guo@fengjr.com>
2019-08-28 18:04:19 +0800

    读书笔记
    Signed-off-by: xupeng.guo <xupeng.guo@fengjr.com>

--- algorithm/src/main/java/com/xcn/code/sort/algorthm/MaoPaoSortAlgorithm.java
+++ algorithm/src/main/java/com/xcn/code/sort/algorthm/MaoPaoSortAlgorithm.java
@@ -15,13 +15,19 @@ public class MaoPaoSortAlgorithm extends AbstractSortAlgorithm {
     @Override
     protected int[] process(int[] datas) {
         int temp = 0;
-        for (int i = 0; i < datas.length-1; i++) {
+        for (int i = 0; i < datas.length - 1; i++) {
+            // 由于有了swapFlag 所以最优时间复杂度为O(n)
+            boolean swapFlag = false;
             for (int k = 0; k < datas.length - i - 1; k++) {
                 if (datas[k] > datas[k + 1]) {
                     temp = datas[k + 1];
                     datas[k + 1] = datas[k];
                     datas[k] = temp;
+                    swapFlag = true;
                 }
+            }
+            if (swapFlag) {
+                return datas;
             }
         }
         return datas;
