10252a894718f81cc036470668811c3a1d4cfb05
xupeng.guo <xupeng.guo@fengjr.com>
2019-10-08 17:25:06 +0800

    读书笔记
    Signed-off-by: xupeng.guo <xupeng.guo@fengjr.com>

--- algorithm/src/main/java/com/xcn/code/LinkedTableConvert.java
+++ algorithm/src/main/java/com/xcn/code/LinkedTableConvert.java
@@ -4,14 +4,21 @@ import com.xcn.bean.Node;
 
 /**
  * 单向链表反转
+ *
  * @author: xupeng.guo
  * @date: 2019/3/19
  * @description
  */
 public class LinkedTableConvert {
 
-    public static Node convert(Node node){
-        if(!node.hasNext()){
+    /**
+     * 递归
+     *
+     * @param node
+     * @return
+     */
+    public static Node convert(Node node) {
+        if (!node.hasNext()) {
             return node;
         }
         Node n1 = convert(node.getNextNode());
@@ -20,6 +27,33 @@ public class LinkedTableConvert {
         return n1;
     }
 
+
+    /**
+     * 循环
+     *
+     * @param head
+     * @return
+     */
+    public static Node convert2(Node head) {
+        //单链表为空或只有一个节点，直接返回原单链表
+        if (head == null || head.getNextNode() == null) {
+            return head;
+        }
+        //前一个节点指针
+        Node preNode = null;
+        //当前节点指针
+        Node curNode = head;
+        //下一个节点指针
+        Node nextNode = null;
+
+        while (curNode != null) {
+            nextNode = curNode.getNextNode();//nextNode 指向下一个节点
+            curNode.setNextNode(preNode);//将当前节点next域指向前一个节点
+            preNode = curNode;//preNode 指针向后移动
+            curNode = nextNode;//curNode指针向后移动
+        }
+        return preNode;
+    }
 
     public static void main(String[] args) {
         Node n1 = new Node("1");
@@ -31,7 +65,7 @@ public class LinkedTableConvert {
         n3.setNextNode(n4);
         printf(n1);
         System.out.println("---------");
-        n1 = LinkedTableConvert.convert(n1);
+        n1 = LinkedTableConvert.convert2(n1);
         printf(n1);
     }
 
@@ -39,6 +73,6 @@ public class LinkedTableConvert {
         Node tempN = n;
         do {
             System.out.println(tempN);
-        } while ((tempN = tempN.getNextNode()) != null) ;
+        } while ((tempN = tempN.getNextNode()) != null);
     }
 }
